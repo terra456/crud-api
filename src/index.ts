79e8a2f228b5db6e5389014e463c9f2baae684f3
terra456 <terra.456@yandex.ru>
2023-01-11 23:20:33 +0500

    feat: create server with nothing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import http from "node:http";
+
+const server: http.Server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+server.listen(4000,'localhost', 1, () => {
+  console.log('listening port 4000');
+});
