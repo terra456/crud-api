0613f13a0cf10a71a243b04e001688cf72d7c4aa
terra456 <terra.456@yandex.ru>
2023-01-13 12:52:41 +0500

    feat: add functions for get all users, one user and create user

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import http from "node:http";
+import { UserType } from "./modules/types.js";
 import UsersData from "./modules/usersData.ts";
 
 const usersData = new UsersData();
 
 const server: http.Server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  console.log(req.url, req.headers['content-type']);
 
   if (req.url.startsWith('/api/users')) {
+    
     switch(req.method) {
       case 'GET':
-       if (req.url == '/api/users') {
+       if (req.url === '/api/users') {
         // console.log(usersData.getUsers);
         usersData.getUsers()
         .then((data: string) => {
@@ -17,10 +19,64 @@ const server: http.Server = http.createServer((req, res) => {
             res.write(data);
             res.end();
         });
+       } else {
+        const newId = req.url.replace('/api/users/', '');
+        usersData.getUser(newId)
+        .then((data: UserType) => {
+          console.log(data);
+          res.statusCode = 201;
+          res.setHeader('Content-type', 'text/json');
+          res.write(JSON.stringify(data));
+          res.end();
+        })
+        .catch((e: Error) => {
+          console.log(e.message);
+          res.statusCode = 404;
+          res.write(e.message);
+          res.end();
+        });
        }
        break;
       case 'POST':
-        usersData.createUser({});
+        if (req.url === '/api/users' && req.headers['content-type'] === 'application/json') {
+          let rawData = '';
+          req.on('data', (chunk) => {
+            rawData += chunk;
+          });
+          req.on('end', () => {
+            try {
+              const parsedData = JSON.parse(rawData);
+              console.log(parsedData);
+              usersData.createUser(parsedData)
+                .then((data: UserType) => {
+                  console.log(JSON.stringify(data));
+                  res.statusCode = 201;
+                  res.setHeader('Content-type', 'text/json');
+                  res.write(JSON.stringify(data));
+                  res.end();
+                })
+                .catch((err: Error) => {
+                  res.statusCode = 400;
+                  res.write(err.message);
+                  res.end();
+                  console.error('ERR ', err.message);
+                });
+            } catch (e) {
+              res.statusCode = 400;
+              res.write(e.message);
+              res.end();
+              console.error('ERROR ', e.message);
+            }
+          });
+        }
+        // res.setHeader('Content-type', 'text/json');
+        // res.write('ehfgoeijfqwkdf[');
+        // res.end();
+        // req.setEncoding('utf8');
+        
+        
+        
+        // usersData.createUser({});
         break;
     }
   }
--- src/modules/usersData.ts
+++ src/modules/usersData.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 import { UserType } from './types.ts';
 
 class UsersData {
@@ -11,13 +12,48 @@ class UsersData {
     return JSON.stringify(this.users);
   }
 
-  getUser(id: any) {
-    return this.users[id];
+  getUser(str: string) {
+    const answer = new Promise((res, rej) => {
+      if (uuidValidate(str)) {
+        const user = this.users.find((el) => el.id === str);
+        if (user) {
+          res(user);
+        } else {
+          rej(new Error('user not find'));
+        }
+      } else {
+        rej(new Error('userId is invalid'));
+      }
+    });
+    return answer;
   }
 
-  createUser(obj: any) {
+  async createUser(obj: any) {
+    const user: UserType = this.checkUserFields(obj);
+    const answer = new Promise((res, rej) => {
+      if (user) {
+        this.users.push(user);
+        res(user);
+      } else {
+        rej(new Error('request does not contain required fields'));
+      }
+    });
+    return answer;
+  }
+
+  private checkUserFields(obj: any) {
     const user = obj;
-    this.users.push(user);
+    if (user.name && Number(user.age) && user.hobbies) {
+      const newUser: UserType = {
+        id: uuidv4(),
+        name: user.name,
+        age: Number(user.age),
+        hobbies: user.hobbies.split(','),
+      }
+      return newUser;
+    } else {
+      return false;
+    }
   }
 
   updateUser(obj: any) {
