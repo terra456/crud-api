9e9f414668453b139646f2fad61322dbf0955347
terra456 <terra.456@yandex.ru>
2023-01-16 15:32:55 +0500

    feat: test for change user data

---
+++ tests/updateUser.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest";
+import server from "../src/server.ts";
+
+const app = server;
+
+describe('POST update user', () => {
+  let user;
+  it('create new user', async () => {
+    const response = await request(app)
+      .post('/api/users')
+      .send({ name: 'Manny', age: 2, hobbies: 'one, two' })
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(201);
+    user = JSON.parse(response.text);
+  });
+
+  it('responds on incorrect id', async () => {
+    const response = await request(app)
+      .get('/api/users/123')
+      .send({name:"john"})
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(400);
+  });
+    
+  it('responds on existing id', async () => {
+    const response = await request(app)
+      .get('/api/users/a2910d1f-81b4-43e3-b503-852a87855b1c')
+      .send({name:"john"})
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(404);
+  });
+
+  it('respond to change with new data', async () => {
+    const response = await request(app)
+      .put(`/api/users/${user.id}`)
+      .send({name:"john"})
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(200);
+    expect(response.text).toMatch(/john/);
+  });
+
+  afterAll(() => {
+    app.close()
+  });
+});
+
