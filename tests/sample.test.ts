573135ee3a3baacd7e905970973c0ca652efd87e
terra456 <terra.456@yandex.ru>
2023-01-16 13:05:12 +0500

    feat: test of simple responses

---
+++ tests/sample.test.ts
@@ -0,0 +1,26 @@
+import request from "supertest";
+// import superagent from 'superagent';
+import server from "../src/server.ts";
+
+const app = server;
+
+describe('GET /users', () => {
+  it('responds with correct status', async () => {
+    const response = await request(app)
+      .get('/api/users')
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(200);
+  });
+
+  it('responds with json', async () => {
+    const response = await request(app)
+      .get('/api/kfjepf')
+      .set('Accept', 'application/json')
+    expect(response.status).toEqual(404);
+  });
+
+  afterAll(() => {
+    app.close()
+  });
+});
+
